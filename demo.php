c80a15e1e6b098755e3980ec90e19bc007d79ae2
primalrambler <primal.rambler@gmail.com>
2016-08-29 14:16:41 -0500

    demo / lesson example problem file

---
+++ demo.php
@@ -0,0 +1,35 @@
+<?php
+
+$person = [
+	'first_name' => 'Dan',
+	'last_name' => 'Carroll',
+	'email' => 'dcarrll@gmail.com',
+	'phone' => '123-456-7890'
+];
+
+$test = [
+	'person1' => [
+	'first_name' => 'Dan',
+	'last_name' => 'Carroll',
+	'email' => 'dcarrll@gmail.com',
+	'phone' => '123-456-7890'
+	],
+
+	'person2' => [
+	'first_name' => 'Joe',
+	'last_name' => 'Montana',
+	'email' => 'fortyniner@gmail.com',
+	'phone' => '123-456-7890'
+	],
+
+	'person3' => [
+	'first_name' => 'Dora',
+	'last_name' => 'Explora',
+	'email' => 'lost@gmail.com',
+	'phone' => '123-456-7890'
+	],
+
+
+];
+
+print_r($test);
