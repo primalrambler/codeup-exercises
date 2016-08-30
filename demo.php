c735467c8ec1af0500dccd18cc47c76266617cef
primalrambler <primal.rambler@gmail.com>
2016-08-30 14:12:53 -0500

    checking for variable isset

--- demo.php
+++ demo.php
@@ -6,3 +6,13 @@ var_dump($argc);
 echo PHP_EOL;
 echo "\$argv is an arrary of all the arguments".PHP_EOL;
 var_dump($argv);
+
+echo "the filename of the script is $argv[0]".PHP_EOL;
+
+if (isset($argv[1])){
+	$firstArgument = $argv[1];
+} else {
+	$firstArgument = "no value specified";
+}
+
+echo "The first argument is $firstArgument".PHP_EOL;
