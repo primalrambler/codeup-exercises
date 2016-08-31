7130527374037493f77aaa004592c203e5648f74
primalrambler <primal.rambler@gmail.com>
2016-08-31 11:48:21 -0500

    added fizzbuzz check for #'s divisible by 3 and 5

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -8,7 +8,9 @@ $fizzMultiple = 3;
 $buzzMultiple = 5;
 
 for ($i = $min; $i <= $max; $i+=1){
-	if ($i % $fizzMultiple == 0){
+	if ($i % fizzMultiple == 0 && $i % buzzMultiple == 0 ) {
+		fwrite(STDOUT,'FIZZBUZZ'.PHP_EOL);
+	} elseif ($i % $fizzMultiple == 0){
 		fwrite(STDOUT,'FIZZ'.PHP_EOL);
 	} elseif ($i % $buzzMultiple == 0) {
 		fwrite(STDOUT,'BUZZ'.PHP_EOL);
