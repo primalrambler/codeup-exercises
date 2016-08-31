9b97a2a8e20a10cbc1eb246844c1a557c4f8ffdd
primalrambler <primal.rambler@gmail.com>
2016-08-31 11:18:13 -0500

    added buzz print logic. originally left out of previous commits.

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,4 +1,18 @@
+<?php 
 
-//Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” 
+//Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” and mult of 5 "buzz" 
 
-<?php $max = 100; $min = 1; $fizzMultiple = 3; for ($i = $min; $i <= $max; $i+=1){	($i % $fizzMultiple == 0) ? fwrite(STDOUT,'FIZZ'.PHP_EOL) : fwrite(STDOUT,$i.PHP_EOL);}
+$max = 100; 
+$min = 1; 
+$fizzMultiple = 3; 
+$buzzMultiple = 5;
+
+for ($i = $min; $i <= $max; $i+=1){
+	if ($i % $fizzMultiple == 0){
+		fwrite(STDOUT,'FIZZ'.PHP_EOL);
+	} elseif ($i % $buzzMultiple == 0) {
+		fwrite(STDOUT,'BUZZ'.PHP_EOL);
+	} else {
+		fwrite(STDOUT,$i.PHP_EOL);
+	}
+}
