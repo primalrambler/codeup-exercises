a866fb65f6db76bdf4bf20357f901812fa1f1899
primalrambler <primal.rambler@gmail.com>
2016-08-31 12:28:30 -0500

    prints out each array elment, including elments from internal arrays

--- foreach.php
+++ foreach.php
@@ -2,18 +2,13 @@
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
-foreach($things as $thing){
-	if (is_integer($thing)){
-		echo "$integer type\n";
-	} elseif (is_float($thing)) {
-		echo "float type\n";
-	} elseif (is_bool($thing)){
-	 echo "boolean type\n";
-	} elseif (is_array($thing)) {
-		echo "array type\n";
-	} elseif (is_null($thing)) {
-		echo "null type\n";
-	} elseif (is_string($thing)) {
-	  echo "string type\n";
+
+foreach($things as $thing) {
+	if (is_array($thing)) {
+		foreach ($thing as $subthing) {
+			echo "$subthing".PHP_EOL;
+		}
+	} else {
+		echo "$thing".PHP_EOL;
 	}
 }
