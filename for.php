b355a23b0320ef0263efcf2964add72704f3e0d6
primalrambler <primal.rambler@gmail.com>
2016-08-31 11:01:29 -0500

    added inputs for begin/end numbers and 'count by' input. count by is checked to see if it will count evenly between the two endpoints.

--- for.php
+++ for.php
@@ -4,28 +4,46 @@
 // first number then second number
 //validate user input
 
-
+//get first number
 do {
 	fwrite(STDOUT,"Enter the first number: ");
 	$firstNum = trim(fgets(STDIN));
 } while (!is_numeric($firstNum));
 
+//get second number
 do {
 	fwrite(STDOUT,"Enter the second number: ");
 	$secondNum = trim(fgets(STDIN));
 } while (!is_numeric($secondNum));
 
+
+//get number to count by
+do {
+	fwrite(STDOUT,"Count by ? ");
+	$xcrement = trim(fgets(STDIN));
+
+	//if user just hits enter assign +/- 1
+	if (is_null($xcrement)){
+		($firstNum >= $secondNum) ? $xcrement = -1 : $xcrement = 1;
+		break;
+	}
+	//check if increment will actually count to the number
+	$isDivisible = ($firstNum - $secondNum) % $xcrement == 0;
+} while ((!is_numeric($xcrement)) || !$isDivisible);
+
+//assign output variable, needed since for loop
+//counts up or down
 $outNum = $firstNum;
 
+//countDOWN to second number
+if ($firstNum >= $secondNum) {
+	for ( $i = $firstNum; $i >= $secondNum; $i += ($xcrement * -1)){
+	fwrite(STDOUT,$i.PHP_EOL);
+	}
 
-//set value to decrement/increment by
-($firstNum >= $secondNum) ? $xcrement = -1 : $xcrement = 1;
-
-//loop through and increment or decrement by 1
-
-for ( $i = 0; $i <= abs($firstNum - $secondNum); $i += 1 ){
-	fwrite(STDOUT,$outNum.PHP_EOL);
-	$outNum += $xcrement;
+//countUP to second number	
+} else {
+	for ( $i = $firstNum; $i <= $secondNum; $i += $xcrement){
+	fwrite(STDOUT,$i.PHP_EOL);
+	}
 }
-
-//print to screen
