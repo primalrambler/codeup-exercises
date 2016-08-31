bff6c7a4ea2a17b148d7d67de85b798de2a0b3a3
primalrambler <primal.rambler@gmail.com>
2016-08-31 11:10:46 -0500

    add fizz and buzz mulitple variables so you can pick which multiple to FIZZ and which to BUZZ

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,4 +1,4 @@
 
 //Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” 
 
-<?php $max = 100; $min = 1; for ($i = $min; $i <= $max; $i+=1){	($i % 3 == 0) ? fwrite(STDOUT,'FIZZ'.PHP_EOL) : fwrite(STDOUT,$i.PHP_EOL);}
+<?php $max = 100; $min = 1; $fizzMultiple = 3; for ($i = $min; $i <= $max; $i+=1){	($i % $fizzMultiple == 0) ? fwrite(STDOUT,'FIZZ'.PHP_EOL) : fwrite(STDOUT,$i.PHP_EOL);}
