b0af5d38acbd4599fbf27a95f78fa47f7ea23fd4
primalrambler <primal.rambler@gmail.com>
2016-09-01 10:59:03 -0500

    class exercise, use switch case to print day of week

---
+++ switch.php
@@ -0,0 +1,25 @@
+ <?php
+
+ // Set the default timezone
+ date_default_timezone_set('America/Chicago');
+
+ // Get Day of Week as number
+ // 1 (for Monday) through 7 (for Sunday)
+ $dayOfWeek = date('N');
+
+ switch($dayOfWeek) {
+    case 1:
+        echo "Monday".PHP_EOL;
+    case 2:
+     	echo "Tuesday".PHP_EOL;
+ 	case 3:
+        echo "Wednesday".PHP_EOL;
+     case 4:
+     	echo "Thursday".PHP_EOL;
+ 	case 5:
+        echo "Friday".PHP_EOL;
+     case 6:
+     	echo "Saturday".PHP_EOL;
+     case 7:
+     	echo "Sunday".PHP_EOL;
+ }
