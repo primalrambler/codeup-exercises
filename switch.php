7eebb1d911c954cfca41c023586958059821777c
primalrambler <primal.rambler@gmail.com>
2016-09-01 11:03:33 -0500

    added if block to create same effect as switch case. odd behavior on switch case where I'm getting a leading space.

--- switch.php
+++ switch.php
@@ -10,16 +10,45 @@
  switch($dayOfWeek) {
     case 1:
         echo "Monday".PHP_EOL;
+        break;
     case 2:
      	echo "Tuesday".PHP_EOL;
+     	break;
  	case 3:
         echo "Wednesday".PHP_EOL;
-     case 4:
+        break;
+    case 4:
      	echo "Thursday".PHP_EOL;
+     	break;
  	case 5:
         echo "Friday".PHP_EOL;
+        break;
      case 6:
      	echo "Saturday".PHP_EOL;
+     	break;
      case 7:
      	echo "Sunday".PHP_EOL;
+     	break;
+ }
+
+ if ($dayOfWeek == 1){
+ 	echo "Monday".PHP_EOL;
+ }
+  if ($dayOfWeek == 2){
+ 	echo "Tuesday".PHP_EOL;
+ }
+  if ($dayOfWeek == 3){
+ 	echo "Wednesday".PHP_EOL;
+ }
+  if ($dayOfWeek == 4){
+ 	echo "Thursday".PHP_EOL;
+ }
+  if ($dayOfWeek == 5){
+ 	echo "Friday".PHP_EOL;
+ }
+  if ($dayOfWeek == 6){
+ 	echo "Saturday".PHP_EOL;
+ }
+  if ($dayOfWeek == 7){
+ 	echo "Sunday".PHP_EOL;
  }
