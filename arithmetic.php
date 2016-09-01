d1814bab486389403cc6c5fe127f7e6d56ae541b
primalrambler <primal.rambler@gmail.com>
2016-09-01 12:00:58 -0500

    added automatic testing to check arithmetic.

--- arithmetic.php
+++ arithmetic.php
@@ -7,17 +7,40 @@ function add($a, $b)
 
 function subtract($a, $b)
 {
-    // Add code here
+    return $a - $b;
 }
 
 function multiply($a, $b)
 {
-    // Add code here
+    return $a * $b;
 }
 
 function divide($a, $b)
 {
-    // Add code here
+    return ($b == 0) ? "cannot divide by zero" : $a / $b;
+
 }
 
 // Add code to test your functions here
+
+$testNumberArray = [
+	'integer' => [37, 110],
+	'decimal' => [3.4897, 57.8929],
+	'fractional' => [0.8842, 0.004837],
+];
+
+function testNumbers($numberArray,$signIndex0, $signIndex1)
+{
+	foreach ($numberArray as $numType => $numArray) {
+		echo "Testing values of type: {$numType}".PHP_EOL;
+		echo $numArray[0] * $signIndex0." + ".$numArray[1] * $signIndex1." = ".add($numArray[0] * $signIndex0,$numArray[1] * $signIndex1).PHP_EOL;
+		echo $numArray[0] * $signIndex0." - ".$numArray[1] * $signIndex1." = ".subtract($numArray[0] * $signIndex0,$numArray[1] * $signIndex1).PHP_EOL;
+		echo $numArray[0] * $signIndex0." * ".$numArray[1] * $signIndex1." = ".multiply($numArray[0] * $signIndex0,$numArray[1] * $signIndex1).PHP_EOL;
+		echo $numArray[0] * $signIndex0." / ".$numArray[1] * $signIndex1." = ".divide($numArray[0] * $signIndex0,$numArray[1] * $signIndex1).PHP_EOL;	
+	}
+}
+
+testNumbers($testNumberArray,1,1);
+testNumbers($testNumberArray,-1,-1);
+testNumbers($testNumberArray,-1,1);
+testNumbers($testNumberArray,1,-1);
