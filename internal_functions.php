da0609475c2a136b87b95cce9e1cb2bce3075269
primalrambler <primal.rambler@gmail.com>
2016-09-01 15:25:06 -0500

    add swith/case to handle special cases of empty strings & arrays, null and boolean

--- internal_functions.php
+++ internal_functions.php
@@ -2,6 +2,29 @@
 
 // TODO: Create your inspect() function here
 
+function inspect($data){
+
+	$msg = "The ".gettype($data)." is ";
+
+	switch (gettype($data)){
+		case 'array' :
+			empty($data) ? $msg = "The value is an empty array.".PHP_EOL : $msg .= "[".implode(", ",$data)."]".PHP_EOL;
+			break;
+		case 'NULL' :
+			$msg = "The value is NULL".PHP_EOL;
+			break;
+		case 'boolean' :
+			$data ? $msg .= "TRUE".PHP_EOL : $msg .= "FALSE".PHP_EOL;
+			break;
+		case 'string' :
+			empty($data) ? $msg = "The string is empty.".PHP_EOL : $msg .= $data.PHP_EOL;
+			break;
+		default :
+			$msg .= $data.PHP_EOL;
+	}
+	return $msg;
+}
+
 // Do not mofify these variables!
 $string1 = "I'm a little teapot";
 $string2 = '';
@@ -18,23 +41,34 @@ $null = NULL;
 // TODO: After each echo statement, use inspect() to output the variable's type and its value
 
 echo 'Inspecting $num1:' . PHP_EOL;
+echo inspect($num1).PHP_EOL;
 
 echo 'Inspecting $num2:' . PHP_EOL;
+echo inspect($num2).PHP_EOL;
 
 echo 'Inspecting $num3:' . PHP_EOL;
+echo inspect($num3).PHP_EOL;
 
 echo 'Inspecting $num4:' . PHP_EOL;
+echo inspect($num4).PHP_EOL;
 
 echo 'Inspecting $null:' . PHP_EOL;
+echo inspect($null).PHP_EOL;
 
 echo 'Inspecting $bool1' . PHP_EOL;
+echo inspect($bool1).PHP_EOL;
 
 echo 'Inspecting $bool2' . PHP_EOL;
+echo inspect($bool2).PHP_EOL;
 
 echo 'Inspecting $string1' . PHP_EOL;
+echo inspect($string1).PHP_EOL;
 
 echo 'Inspecting $string2' . PHP_EOL;
+echo inspect($string2).PHP_EOL;
 
 echo 'Inspecting $array1' . PHP_EOL;
+echo inspect($array1).PHP_EOL;
 
 echo 'Inspecting $array2' . PHP_EOL;
+echo inspect($array2).PHP_EOL;
