792ca7db7f37b7dbf111b87ccce80b495ede19f2
primalrambler <primal.rambler@gmail.com>
2016-09-02 11:08:49 -0500

    checks an array for a name and returns 'TRUE' or 'FALSE'

--- search-arrays.php
+++ search-arrays.php
@@ -3,3 +3,12 @@
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+function trawler($array, $query)
+{
+	$result = array_search($query, $array);
+	return (is_numeric($result)) ? 'TRUE' : 'FALSE';
+}
+
+$testName = 'Bob';
+echo trawler($names, $testName).PHP_EOL;
