a78404245156138a024880de23df376b3ad9ad31
primalrambler <primal.rambler@gmail.com>
2016-09-02 11:26:45 -0500

    added comparing two arrays. added functionality to check which is larger so that one controlled then number of keys to search through.

--- search-arrays.php
+++ search-arrays.php
@@ -7,8 +7,25 @@ $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 function trawler($array, $query)
 {
 	$result = array_search($query, $array);
-	return (is_numeric($result)) ? 'TRUE' : 'FALSE';
+	return (is_numeric($result));
 }
 
-$testName = 'Bob';
-echo trawler($names, $testName).PHP_EOL;
+
+
+
+function compareThem($biggerArray, $smallerArray) {
+	$similarElements = 0;
+
+	foreach ($biggerArray as $key => $value) 
+	{
+		if (trawler($smallerArray,$value)){
+			$similarElements +=1;
+		}
+	}
+	return "There are ".$similarElements." similar elements".PHP_EOL;
+}
+
+
+echo (count($names) > count($compare)) ? compareThem($names, $compare) : compareThem($compare, $names); 
+
+
