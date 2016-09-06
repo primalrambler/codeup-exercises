11f9dca44309e1d32a62ff91eb70392ecc0fe912
primalrambler <primal.rambler@gmail.com>
2016-09-06 11:40:41 -0500

    read a file and process contact info into an array. add phone number formatting

---
+++ contact-parser.php
@@ -0,0 +1,53 @@
+<?php
+
+$filename = 'contacts.txt';
+
+
+function getFileContents ($filename)
+{
+    $handle = fopen($filename, 'r');
+
+	$fileContents = fread($handle, filesize($filename));
+
+	fclose($handle);
+
+	return $fileContents;
+}
+
+function phoneNumberFormat ($phoneNumber)
+{
+	$phoneNumber = substr($phoneNumber, 0,3).'-'.substr($phoneNumber, 3, 3).'-'.substr($phoneNumber, 6);
+	return $phoneNumber;
+
+}
+
+
+function parseContacts($fileContents)
+{
+    //initial array set up
+    $contacts = array();
+    $contacts = explode("\n", $fileContents);
+    array_pop($contacts);
+
+    //split out name and number into associative array
+
+    foreach ($contacts as $key => $value) {
+    	$temp = explode ("|",$value);
+    	$contact['name'] = $temp[0];
+    	$contact['number'] = phoneNumberFormat($temp[1]);
+    	$contacts[$key] = $contact;
+    }
+
+
+    var_dump($contacts);
+
+    // todo - read file and parse contacts
+
+    // return $contacts;
+}
+
+$fileContents = getFileContents($filename);
+parseContacts($fileContents);
+
+// var_dump(parseContacts('contacts.txt'));
+// 
