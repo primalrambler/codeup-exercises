bafa04331a13d749fdf7e2a1f00d59f9b5db344f
primalrambler <primal.rambler@gmail.com>
2016-09-07 10:35:11 -0500

    added date and time stamp to log message.

--- logger.php
+++ logger.php
@@ -1,16 +1,23 @@
 <?php
 
-$datestamp = date('Y-m-d');
-$timestamp = date('H:i:s');
+function getDateStamp ()
+{
+	return $datestamp = date('Y-m-d');	
+}
+
+function getTimeStamp()
+{
+	return $timestamp = date('H:i:s');
+}
+
 
 function logMessage($logLevel, $message)
 {
     $filename = 'test.log';
 	$handle = fopen($filename, 'a');
-	fwrite($handle, PHP_EOL . $logLevel." ".$message);
+	fwrite($handle, PHP_EOL .getDateStamp()." ".getTimeStamp()." ". $logLevel." ".$message);
 }
 
 logMessage("INFO", "This is an info message.");
 logMessage("ERROR", "This is an info message.");
-echo $datestamp.PHP_EOL;
-echo $timestamp.PHP_EOL;
+
